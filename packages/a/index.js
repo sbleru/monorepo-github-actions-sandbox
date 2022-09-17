36b670688aa56162b14cfbaa7a76bf8f2cf72fd9
sbleru <usareru@gmail.com>
2022-09-18 01:55:04 +0900

    feat: Add another uuid package

--- packages/a/index.js
+++ packages/a/index.js
@@ -1,6 +1,7 @@
 const common = require('../common')
 
 common.print();
+common.print2();
 
 // const moduleA = require('../common')
 // console.log(moduleA) // -> a
--- packages/common/index.js
+++ packages/common/index.js
@@ -1,8 +1,12 @@
 const uuid = require('uuid');
+const uuidv4 = require('uuidv4');
 
 module.exports = {
   print: () => {
-    console.log(`This is common. ${uuid.v4()}`);
+    console.log(`This is common print. ${uuid.v4()}`);
+  },
+  print2: () => {
+    console.log(`This is common print2. ${uuidv4.uuid()}`)
   }
 }
 // print();
